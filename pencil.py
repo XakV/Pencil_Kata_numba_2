9b6958615651340727da23c252a995f723756147
aikidouke <zach@mailup.net>
2018-11-16 11:29:26 -0500

    Created pencil object in pencil.py. First test passed!

---
+++ pencil.py
@@ -0,0 +1,10 @@
+'''
+Pencil Kata - Pillar Technology Apprentice Application
+Author: Zach Villers
+Started: 16 Nov 2018
+Requirements: https://github.com/PillarTechnology/kata-pencil-durability
+Test Framework: pytest
+'''
+
+def pencil():
+    return pencil
---
+++ test_pencil.py
@@ -0,0 +1,11 @@
+'''
+Tests for Pillar Tech Pencil Kata - using pytest
+Requirements:
+ - Pencil must be able to write text on a sheet of paper
+'''
+
+import pencil
+
+# Tests for Pencil Requirements
+def test_pencil():
+    assert pencil
