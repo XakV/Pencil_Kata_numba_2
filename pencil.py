17feb035e24f34b6fd904f604bb4dd50129369ff
aikidouke <zach@mailup.net>
2018-11-16 12:24:59 -0500

    Pencil is now in a separate class with two methods, pencil (boolean represents the pencil) and writes (boolean represents the pencil's ability to write)

--- pencil.py
+++ pencil.py
@@ -6,14 +6,20 @@ Requirements: https://github.com/PillarTechnology/kata-pencil-durability
 Test Framework: pytest
 '''
 
-def pencil():
-    return pencil
+# Pencil is a writing implement
+
+class Pencil(object):
+    def pencil(self):
+        return Pencil.pencil
+
+    def writes(self):
+        return Pencil.writes
 
 
 # Paper is the surface the pencil creates text on
 
-class Paper:
- 
+class Paper(object):
+
     def paper(self):
         return Paper.paper
 
--- test_pencil.py
+++ test_pencil.py
@@ -1,18 +1,20 @@
 '''
 Tests for Pillar Tech Pencil Kata - using pytest
-Requirements:
+Kata Specification:
  - Pencil must be able to write text on a sheet of paper
  - Paper should reflect that text is written
+ - Text written by the pencil should always be appended to existing text on the paper.
 '''
 
-import pencil
+from pencil import Pencil
 from pencil import Paper
 
-# Tests for Pencil Requirements
+# Tests for Pencil Specs
 def test_pencil():
-    assert pencil
+    assert Pencil.pencil
+    assert Pencil.writes
 
+# Tests for Paper Specs
 def test_paper():
     assert Paper.paper
     assert Paper.text
-    
