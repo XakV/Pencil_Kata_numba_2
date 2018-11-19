fafce1f35468435e35752fa535cabb3f5091dfac
aikidouke <zach@mailup.net>
2018-11-19 10:32:54 -0500

    Calling a do-over on this. I realized that my starting tests were more granular than the requirements. Starting with a clean slate.

--- pencil.py
+++ pencil.py
@@ -6,34 +6,3 @@ Requirements: https://github.com/PillarTechnology/kata-pencil-durability
 Test Framework: pytest
 '''
 
-
-class Pencil:
-    """A Pencil is a tool for writing and drawing"""
-
-    def pencil(self):
-        """The base object of pencil"""
-        return self.pencil
-
-    def writes(self):
-        """A pencil that can be used for writing."""
-        return self.writes
-
-
-class Paper:
-    """Paper is the given medium for writing"""
-
-    def paper(self):
-        """Base object for paper medium"""
-        return self.paper
-
-    def text(self):
-        """Text is created by a pencil writing on paper"""
-        return self.text
-
-    def filename(self):
-        """Papers are kept as named files"""
-        filename = self.filename
-
-    def filepath(self):
-        """files are kepts in named locations known as a file_path"""
-        filepath = self.filepath
--- test_pencil.py
+++ test_pencil.py
@@ -7,34 +7,3 @@ Kata Specification:
 '''
 
 from pencil import Pencil
-from pencil import Paper
-
-def test_pencil_is_a_physical_object():
-    """Testing instance of pencil class"""
-    assert Pencil.pencil
-
-def test_pencil_can_write():
-    """Does pencil have the ability to write"""
-    assert Pencil.writes
-
-def test_paper_is_a_physical_object():
-    """
-    Testing instance of paper class
-    Text is recorded in ASCII characters, is retreivable, and can not overwrite itself
-    """
-    assert Paper.paper
-
-def test_paper_has_text():
-    """
-    Text is captured in ASCII format
-    Text can not destroy itself
-    """
-    assert Paper.text
-
-def test_paper_can_be_found():
-    """
-    Files in Unix-Like systems have a filename
-    Files in Unix-Like systems can be found at an explicit path
-    """
-    assert Paper.filename
-    assert Paper.filepath
