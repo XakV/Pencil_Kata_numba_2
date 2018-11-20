4ad97e08b0f446051ecbce81abadec23beba4ad6
aikidouke <zach@mailup.net>
2018-11-20 13:27:45 -0500

    Refactored to create a pencil class - pencil can now be created with a durability.

--- pencil.py
+++ pencil.py
@@ -6,9 +6,19 @@ Requirements: https://github.com/PillarTechnology/kata-pencil-durability
 Test Framework: pytest
 '''
 
-def pencil_can_write_text():
-    return pencil_can_write_text
+class pencil():
 
-def write_text_with_pencil(new_text_to_add):
-    new_text_added = new_text_to_add
-    return new_text_added
+    def __init__(self):
+        pass
+
+    def pencil_point_durability(self, durability):
+        pencil_point_durability = durability
+        return self.pencil_point_durability
+
+    def pencil_can_write_text(self):
+        return pencil.pencil_can_write_text
+    
+    def write_text_with_pencil(self, new_text_to_add):
+        pencil.new_text_added = new_text_to_add
+        return pencil.new_text_added
+
--- test_pencil.py
+++ test_pencil.py
@@ -4,14 +4,18 @@ Kata Specification:
  - Pencil must be able to write text on a sheet of paper
  - Paper should reflect that text is written and recorded
  - Text written by the pencil should always be appended to existing text on the paper.
+ - When a pencil is created, it can be provided with a value for point durability.
+ - The pencil will be able to write only a limited number of characters before it goes dull
+
 '''
 
 import pytest
-import pencil
+from pencil import *
 import paper
 
 def test_a_writer_can_use_a_pencil_to_write_text_on_a_sheet_of_paper():
-    assert pencil.pencil_can_write_text()
+    _pencil = pencil()
+    assert _pencil.pencil_can_write_text
 
 def test_paper_should_reflect_that_text_is_written_and_recorded():
     existing_text_on_paper = "Counting flowers on the wall, "
@@ -20,5 +24,13 @@ def test_paper_should_reflect_that_text_is_written_and_recorded():
 def test_text_written_by_pencil_should_always_be_appended_to_existing_text_on_paper():
     existing_text_on_paper = "She sells sea shells"
     new_text_added = " down by the sea shore."
-    assert paper.show_written_text(existing_text_on_paper) + pencil.write_text_with_pencil(new_text_added) == "She sells sea shells down by the sea shore."
-    assert paper.show_written_text(existing_text_on_paper) + pencil.write_text_with_pencil(new_text_added) == paper.view_new_text(existing_text_on_paper, new_text_added)
+    _pencil = pencil()
+    assert paper.show_written_text(existing_text_on_paper) + pencil.write_text_with_pencil(_pencil, new_text_added) == "She sells sea shells down by the sea shore."
+    assert paper.show_written_text(existing_text_on_paper) + pencil.write_text_with_pencil(_pencil, new_text_added) == paper.view_new_text(existing_text_on_paper, new_text_added)
+
+def test_when_a_pencil_is_created_it_has_a_value_for_point_durability():
+    durability = 1
+    new_pencil = pencil()
+    new_pencil.new_pencil_point = new_pencil.pencil_point_durability(durability)
+    assert new_pencil.pencil_point_durability
+
